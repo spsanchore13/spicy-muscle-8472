d3a2a5cb48224585ceed42d4d566a4be5e13d06c
Naresh Rajput <nareshmewada014@gmail.com>
2022-11-09 19:39:46 +0530

    structure complete

---
+++ server/Model/User.model.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    mobile: { type: Number, required: true },
+    username: { type: String, required: true },
+
+}, {
+    timestamp: true
+})
+const UserModel = model("user", userSchema)
+
+module.exports = {
+    UserModel
+}
