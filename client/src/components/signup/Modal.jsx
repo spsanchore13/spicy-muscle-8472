b8690ccc6d89c312dc3623def7b83afef0197f86
Naresh Rajput <nareshmewada014@gmail.com>
2022-11-09 20:57:25 +0530

    signup page added

---
+++ client/src/components/signup/Modal.jsx
@@ -0,0 +1,36 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+export default function SignupModel() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>hello</ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,14 @@
+import { Box, Heading } from '@chakra-ui/layout'
+import React from 'react'
+import SignupModel from '../components/signup/Modal'
+
+const Signup = () => {
+  return (
+    <Box>
+        <Heading>SignupModel</Heading>
+        <SignupModel/>
+    </Box>
+  )
+}
+
+export default Signup
