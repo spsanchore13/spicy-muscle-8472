edb2ec3dabd862cca98739d217161622a7a817ff
Naresh Rajput <nareshmewada014@gmail.com>
2022-11-09 18:28:45 +0530

    add signup in route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
     <div className="App">
-        hello
+      <Routes>
+        <Route path="/products" element={<ProductPage />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
