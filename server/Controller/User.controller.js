4554327d4a4ae3bba55bf0ce6a7b7295769a4283
Naresh Rajput <nareshmewada014@gmail.com>
2022-11-10 19:05:33 +0530

    edit the files

--- server/Controller/User.controller.js
+++ server/Controller/User.controller.js
@@ -4,18 +4,18 @@ const { UserModel } = require("../Model/User.model");
 
 const Signup = async (req, res) => {
     const { email, password, name, username, mobile } = req.body;
-    const userId = await UserModel.find({ email: email })
-    console.log(userId)
+    if (!email || !password || !name || !username || !mobile) {
+        res.status(400).json({ msg: 'Please enter all fields' });
+    }
+    const userId = await UserModel.findOne({ email })
     if (userId) {
-        res.status(403).send({ "message": "User already exists" })
+        res.status(400).send({ "message": "User already exists" })
     } else {
         bcrypt.hash(password, 7, async function (err, hash) {
             if (err) {
                 res.status(500).send({ "message": "something went wrong. please sign up again" })
             }
-            if (!email || !password) {
-                res.send({ "message": "please filled the required fields" })
-            }
+
             const user_data = new UserModel({
                 email: email,
                 password: hash,
@@ -30,13 +30,20 @@ const Signup = async (req, res) => {
 
 }
 const Login = async (req, res) => {
+    console.log(req.body)
     const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400).json({ msg: 'Please enter all fields' });
+    }
     const user = await UserModel.findOne({ email })
+    if (!user) {
+        res.status(400).json({ msg: 'User does not exist' });
+    }
     const hash = user.password
     bcrypt.compare(password, hash, function (err, result) {
         if (result) {
-            var token = jwt.sign({ email: email }, process.env.SECRET_KEY);
-            res.status(200).send({ "message": "login successfully", "token": token, ...user })
+            const token = jwt.sign({ email: email }, process.env.SECRET_KEY, { expiresIn: 60 * 60 });
+            res.status(200).send({ "message": "login successfully", "token": token })
         } else {
             res.status(401).send({ "message": "Invalid Credentials" })
         }
