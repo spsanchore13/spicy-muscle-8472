029a09f32ece50fa44ce5c0c0e56c6decaa2c6a3
shubhfire <singhshubham8585@gmail.com>
2022-11-11 10:46:16 +0530

    home2 update

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
+import Home2 from './components/Home2/Home2';
 import {Navbar} from './components/Navbar/Navbar';
 
 
@@ -17,6 +18,7 @@ function App() {
 
       <Routes>
       <Route exact path="/" element={<Home/>}/>
+      <Route exact path="/home2" element={<Home2/>}/>
       </Routes>
      
 
