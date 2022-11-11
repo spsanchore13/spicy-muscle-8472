6d1e50e4fd537eb85371990d4a216347e20cd66a
Naresh Rajput <nareshmewada014@gmail.com>
2022-11-11 12:32:00 +0530

    add middleware authentication and authorization

---
+++ server/middleware/middleware.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken")
+const authentication = (req, res, next) => {
+    const token = req.headers?.authorization?.split(" ")[1]
+    try {
+        var decoded = jwt.verify(token, process.env.SECRET_KEY);
+        req.body.email = decoded.email
+        next()
+    }
+    catch (err) {
+        res.send("Please login again")
+        console.log(err)
+    }
+}
+
+const authorisation = (permittedrole) => {
+    return async (req, res, next) => {
+        const email = req.body.email
+        const user = await AuthModel.findOne({ email: email })
+        const role = user.role;
+        if (permittedrole.includes(role)) {
+            next()
+        }
+        else {
+            res.send("Not authorised")
+        }
+    }
+}
