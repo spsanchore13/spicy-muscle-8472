5f021efaf7bd7c029c84952ed1fceced736b4941
shubhfire <singhshubham8585@gmail.com>
2022-11-12 09:52:41 +0530

    update home2

--- client/src/Routes/AllRoutes.jsx
+++ client/src/Routes/AllRoutes.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Home from "../components/Home/Home";
+import Home2 from "../components/Home2/Home2";
 import DressesPage from "../pages/DressesPage";
 import ProductPage from "../pages/ProductPage";
 import ProductsDetails from "../pages/ProductsDetailsPage";
@@ -8,6 +10,14 @@ import Signup from "../pages/Signup";
 const AllRoutes = () => {
   return (
     <Routes>
+      <Route exact path="/" element={<Home/>}/>
+      <Route exact path="/home2" element={<Home2/>}/>
+        <Route path="/products" element={<ProductPage />} />
+
+
+        <Route path="/products/:productId" element={<ProductsDetails />} />
+
+        <Route path="/signup" element={<Signup />} />
       <Route path="/shoes" element={<ProductPage />} />
 
       <Route path="/dresses" element={<DressesPage />} />
