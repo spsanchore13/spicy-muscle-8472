f94e8d1ed1aaefb92563b75fd1bfa575fe5f6a0e
Naresh Rajput <nareshmewada014@gmail.com>
2022-11-12 15:51:05 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,10 @@
 
 import "./App.css";
 import AllRoutes from "./Routes/AllRoutes";
-import { Navbar } from "./components/Navbar/Navbar"
 function App() {
   return (
     <div className="App">
-
-    
-   
-
-      <Navbar />
       <AllRoutes />
-
     </div>
   );
 }
--- client/src/Routes/AllRoutes.jsx
+++ client/src/Routes/AllRoutes.jsx
@@ -4,16 +4,16 @@ import DressesPage from "../pages/DressesPage";
 import ProductPage from "../pages/ProductPage";
 import ProductsDetails from "../pages/ProductsDetailsPage";
 import Signup from "../pages/Signup";
-import { Navbar } from "../components/Navbar/Navbar";
 import CheckoutPage from "../pages/CheckoutPage";
 import PaymentSuccessPage from "../pages/PaymentSuccessPage";
 import CartPage from "../pages/CartPage";
+import AdminPage from "../Admin/AdminPage";
 
 const AllRoutes = () => {
   return (
     <>
-      <Navbar />
       <Routes>
+        <Route path="/admin" element={<AdminPage />} />
         <Route path="/shoes" element={<ProductPage />} />
 
         <Route path="/dresses" element={<DressesPage />} />
