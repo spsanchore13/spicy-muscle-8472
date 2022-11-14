4e856070d9799d5ac2182c4e5aeccee61386dd8b
Robingeorge12 <robingsa.btch@gmail.com>
2022-11-14 11:36:27 +0530

    home living page added

---
+++ client/src/Redux/HomeLiving/actionType.js
@@ -0,0 +1,7 @@
+export const DATA_REQUEST = "DATA_REQUEST"
+export const DATA_SUCESS = "DATA_SUCESS"
+export const DATA_FAILURE  = "DATA_FAILURE"
+
+export const POST_REQUEST = "POST_REQUEST"
+export const POST_SUCESS = "POST_SUCESS"
+export const POST_FAILURE  = "POST_FAILURE"
